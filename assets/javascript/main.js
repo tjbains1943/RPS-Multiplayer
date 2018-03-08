d929aa70a0021f7897ac547447359eff2d854263
tj Bains <tjbains1943@yahoo.com>
2018-03-08 11:42:26 -0800

    added comments and removed console.logs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,7 +8,7 @@ var config = {
   messagingSenderId: "543941386248",
 };
 firebase.initializeApp(config);
-
+// variables declared to use globally
 var db = firebase.database();
 var submitCount = 0;
 var thisPlayer;
@@ -22,10 +22,9 @@ var thisPlayer = "";
 var player1Choice = "";
 var player2Choice = "";
 var turn = 1;
-
+// checking db for players already in game
 db.ref("/players/").on("value", function(snapshot) {
 	if (snapshot.child("player1").exists()) {
-		console.log("Player 1 exists");	
 	player1 = snapshot.val().player1;
 		player1Name = player1.name;
 		$("#oneLine").text(player1Name);
@@ -33,17 +32,15 @@ db.ref("/players/").on("value", function(snapshot) {
     $("#name").hide();
     $("#enter").hide(); 
 	} else {
-		console.log("Player 1 does NOT exist");
 		player1 = null;
 		player1Name = "";
     $("#name").show();
     $("#enter").show();
 		$("#oneLine").text("Waiting for Player 1");
-		 $(".roundOutcome").html("Rock-Paper-Scissors");
+		 $(".roundscore").html("Rock-Paper-Scissors");
   }
-  
-	if (snapshot.child("player2").exists()) {
-		console.log("Player 2 exists");
+  // checking for player 2 in db
+	if (snapshot.child("player2").exists()) {	
 		player2 = snapshot.val().player2;
 		player2Name = player2.name;
 		$("#twoLine").text(player2Name);
@@ -51,45 +48,42 @@ db.ref("/players/").on("value", function(snapshot) {
     $("#name").hide();
     $("#enter").hide();
 	} else {
-		console.log("Player 2 does NOT exist");
 		player2 = null;
 		player2Name = "";
     $("#name").show();
     $("#enter").show();
 		$("#playerTwoName").text("Waiting for Player 2");
-		$(".roundOutcome").html("Rock-Paper-Scissors");
+		$(".roundscore").html("Rock-Paper-Scissors");
 		$("#players").html("");
   }
-  
+  // if two players present, start game
 	if (player1 && player2) {
     $(".show").removeClass("border border-dark");
     $(".show").addClass("borderW");
     $(".showT").addClass("border border-dark");	
 		$("#players").html("Waiting on " + player1Name + " to choose...");
   }
-  
+  // if players leaves clear db and html view
 	if (!player1 && !player2) {
 		db.ref("/chat/").remove();
 		db.ref("/turn/").remove();
-		db.ref("/outcome/").remove();
+		db.ref("/score/").remove();
 		$("#textArea").empty();
 		$("#playerPanel1").removeClass("playerPanelTurn");
 		$("#playerPanel2").removeClass("playerPanelTurn");
-		$(".roundOutcome").html("Rock-Paper-Scissors");
+		$(".roundscore").html("Rock-Paper-Scissors");
 		$("#players").html("");
 	}
 });
-
+// if player is removed, get their name and display in chat
 db.ref("/players/").on("child_removed", function(snapshot) {
 	var msg = snapshot.val().name + " has disconnected!";
 	db.ref().child("/chat/").push(msg);
 });
-
+// gets chat messages from db
 db.ref("/chat/").on("child_added", function(snapshot) {
   var chatMsg = snapshot.val();
-  console.log(chatMsg);
   var chatEntry = $("<div>").html(chatMsg);
-  console.log(chatEntry);
 	if (chatMsg.includes("disconnected")) {
 		chatEntry.addClass("text-primary");
 	} else if (chatMsg.includes("joined")) {
@@ -102,12 +96,11 @@ db.ref("/chat/").on("child_added", function(snapshot) {
   $("#textArea").append(chatEntry);
   $("#textArea").scrollTop($("#textArea")[0].scrollHeight);
 });
-
+// if turn 1, notify player 1
 db.ref("/turn/").on("value", function(snapshot) {
 	if (snapshot.val() === 1) {
-		console.log("TURN 1");
 		turn = 1;
-
+// add border to notify player 1
 		if (player1 && player2) {
 			$(".show").removeClass("border-dark");
       $(".show").addClass("borderW");
@@ -115,8 +108,8 @@ db.ref("/turn/").on("value", function(snapshot) {
       $(".showT").removeClass("borderT");
 			$("#players").html("<i>Waiting on " + player1Name + " to choose...</i>");
 		}
-	} else if (snapshot.val() === 2) {
-		console.log("TURN 2");
+	} if (snapshot.val() === 2) {
+
 		turn = 2;
 		if (player1 && player2) {
       $(".show").addClass("border border-dark");
@@ -127,18 +120,17 @@ db.ref("/turn/").on("value", function(snapshot) {
 		}
 	}
 });
-
-db.ref("/outcome/").on("value", function(snapshot) {
-  var cool = $(".roundOutcome");
+// gets score from db and displays it
+db.ref("/score/").on("value", function(snapshot) {
+  var cool = $(".roundscore");
   cool.addClass("text-center text-light bg-danger");
 	cool.html("<br><br><h1>" + snapshot.val() + "</h1>");
 });
-
+// adds player if needed
 $("#enter").on("click", function(event) {
      event.preventDefault();
     if ( ($("#name").val().trim() !== "") && !(player1 && player2) ) {
-      if (player1 === null) {
-        console.log("Adding Player 1");
+      if (player1 === null) {      
         thisPlayer = $("#name").val().trim();
         player1 = {
           name: thisPlayer,
@@ -147,7 +139,7 @@ $("#enter").on("click", function(event) {
           tie: 0,
           choice: ""
         };
-
+// adds player 1 to db or player 2 to db
         db.ref().child("/players/player1").set(player1);
         db.ref().child("/turn").set(1);
         db.ref("/players/player1").onDisconnect().remove();
@@ -155,8 +147,7 @@ $("#enter").on("click", function(event) {
         $("#enter").hide();
         $("#oRock").show();
         $(".intro").prepend("<br><h5>Hello " + thisPlayer + ", you are player 1!</h5>");
-      } else if( (player1 !== null) && (player2 === null) ) {
-        console.log("Adding Player 2");
+      } else if( (player1 !== null) && (player2 === null) ) {        
         thisPlayer = $("#name").val().trim();
         player2 = {
           name: thisPlayer,
@@ -170,11 +161,11 @@ $("#enter").on("click", function(event) {
         $("#name").hide();
         $("#enter").hide();
         $("#tRock").show();
-        $(".intro").prepend("<br><h5>Hello " + thisPlayer + ", you are player 2!</h5>");
-        
+        $(".intro").prepend("<br><h5>Hello " + thisPlayer + ", you are player 2!</h5>");       
       }
     }
   })  
+  // stores chat messages on submit to db
   $("#submit").on("click", function(event) {
     event.preventDefault();
     if ( (thisPlayer !== "") && ($("#talk").val().trim() !== "") ) {
@@ -183,12 +174,11 @@ $("#enter").on("click", function(event) {
       db.ref().child("/chat/").push(msg);
     }
   });
-
+// stores player 1's pick into db
   $("#oRock").on("click", "button", function(event) {
     event.preventDefault();
       if (player1 && player2 && (thisPlayer === player1.name) && (turn === 1) ) {
-      var choice = $(this).text().trim();
-      console.log(choice);
+      var choice = $(this).text().trim();      
       $("#oRock").hide();
       var pickO = $("<h1>");
       pickO.html(choice);
@@ -204,7 +194,7 @@ $("#enter").on("click", function(event) {
       },3000);
     }
   });
-  
+  // stores player 2's pick into db
   $("#tRock").on("click", "button", function(event) {
     event.preventDefault();
     if (player1 && player2 && (thisPlayer === player2.name) && (turn === 2) ) {
@@ -221,61 +211,49 @@ $("#enter").on("click", function(event) {
         $(".panelT").hide();
         $("#tRock").show();
       },3000);
-      
-
     }
   });
-
+// function that compares choices of players and determines winner.
   function rpsCompare() {
     if (player1.choice === "Rock") {
-      if (player2.choice === "Rock") {
-        console.log("tie");
-        db.ref().child("/outcome/").set("Tie game!");
+      if (player2.choice === "Rock") {       
+        db.ref().child("/score/").set("Tie game!");
         db.ref().child("/players/player1/tie").set(player1.tie + 1);
         db.ref().child("/players/player2/tie").set(player2.tie + 1);
-      } else if (player2.choice === "Paper") {
-        console.log("paper wins");
-        db.ref().child("/outcome/").set("Paper wins!");
+      } else if (player2.choice === "Paper") {     
+        db.ref().child("/score/").set("Paper wins!");
         db.ref().child("/players/player1/loss").set(player1.loss + 1);
         db.ref().child("/players/player2/win").set(player2.win + 1);
-      } else { 
-        console.log("rock wins");
-        db.ref().child("/outcome/").set("Rock wins!");
+      } else {       
+        db.ref().child("/score/").set("Rock wins!");
         db.ref().child("/players/player1/win").set(player1.win + 1);
         db.ref().child("/players/player2/loss").set(player2.loss + 1);
       }
     } else if (player1.choice === "Paper") {
-      if (player2.choice === "Rock") {
-        console.log("paper wins");
-        db.ref().child("/outcome/").set("Paper wins!");
+      if (player2.choice === "Rock") {        
+        db.ref().child("/score/").set("Paper wins!");
         db.ref().child("/players/player1/win").set(player1.win + 1);
         db.ref().child("/players/player2/loss").set(player2.loss + 1);
-      } else if (player2.choice === "Paper") {
-        console.log("tie");
-        db.ref().child("/outcome/").set("Tie game!");
+      } else if (player2.choice === "Paper") {       
+        db.ref().child("/score/").set("Tie game!");
         db.ref().child("/players/player1/tie").set(player1.tie + 1);
         db.ref().child("/players/player2/tie").set(player2.tie + 1);
-      } else {
-        console.log("scissors win");
-        db.ref().child("/outcome/").set("Scissors win!");
+      } else {    
+        db.ref().child("/score/").set("Scissors win!");
         db.ref().child("/players/player1/loss").set(player1.loss + 1);
         db.ref().child("/players/player2/win").set(player2.win + 1);
-      }
-  
+      }  
     } else if (player1.choice === "Scissors") {
       if (player2.choice === "Rock") {
-        console.log("rock wins");
-        db.ref().child("/outcome/").set("Rock wins!");
+        db.ref().child("/score/").set("Rock wins!");
         db.ref().child("/players/player1/loss").set(player1.loss + 1);
         db.ref().child("/players/player2/win").set(player2.win + 1);
-      } else if (player2.choice === "Paper") {
-        console.log("scissors win");
-        db.ref().child("/outcome/").set("Scissors win!");
+      } else if (player2.choice === "Paper") {   
+        db.ref().child("/score/").set("Scissors win!");
         db.ref().child("/players/player1/win").set(player1.win + 1);
         db.ref().child("/players/player2/loss").set(player2.loss + 1);
       } else {
-        console.log("tie");
-        db.ref().child("/outcome/").set("Tie game!");
+        db.ref().child("/score/").set("Tie game!");
         db.ref().child("/players/player1/tie").set(player1.tie + 1);
         db.ref().child("/players/player2/tie").set(player2.tie + 1);
       }  
